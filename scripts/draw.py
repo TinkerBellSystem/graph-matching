e199e8b110d27d5e197518ead821b48d7ec55cc4
vagrant <vagrant@fedora27-vbox.localdomain>
2018-11-21 19:28:30 +0000

    graphs and demo

--- scripts/draw.py
+++ scripts/draw.py
@@ -13,4 +13,4 @@ dot_files = os.listdir("../dot")	# The training file names within that directory
 for dot_file in dot_files:
 	hookname = dot_file.split('.')[0]
 	dot_file_path = os.path.join("../dot", dot_file)
-	os.system('dot -Tpng' + dot_file_path + '-o ../img/'+ hookname +'.png')
+	os.system('dot -Tpng ' + dot_file_path + ' -o ../img/'+ hookname +'.png')
